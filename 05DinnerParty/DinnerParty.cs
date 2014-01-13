ef893436a12d1f075a54fcc46943184cd5ed44b1
masumshaikh <smshaikh@hotmail.com>
2014-01-13 15:32:35 +0000

    upload dinner party v2 (ch 6)

---
+++ 05DinnerParty/DinnerParty.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05DinnerParty
+{
+    public class DinnerParty
+    {
+        private const decimal FoodChargePerPerson = 25.0M;
+        private const decimal CostOfAlcPerPerson = 20.0M;
+        private const decimal CostOfSodaPerPerson = 5.0M;
+        private const double fHealthyDiscount = 0.95;
+        private const decimal CostOfFancyDecPerPerson = 15.0M;
+        private const decimal CostOfNormalDecPerPerson = 7.50M;
+        private const decimal FlatFeeFancyDec = 50.0M;
+        private const decimal FlatFeeNormalDec = 30.0M;
+
+
+        private decimal fCostOfBevPerPerson;
+        private double fDiscount;
+        private decimal fCostOfDecPerPerson;
+        private decimal fFlatFeeDecoration;
+
+        public DinnerParty()
+        {
+        }
+
+        public int NumberOfPeople { get; set; }
+        
+        public void SetHealthyOption(bool healthy)
+        {
+            switch (healthy)
+            {
+                case true:
+                    fDiscount = fHealthyDiscount;
+                    fCostOfBevPerPerson = CostOfSodaPerPerson;
+                    break;
+                case false:
+                    fDiscount = 1;
+                    fCostOfBevPerPerson = CostOfAlcPerPerson;
+                    break;
+            }
+        }
+
+        public void SetFancyDecorations(bool fancy)
+        {
+            switch (fancy)
+            {
+                case true:
+                    fCostOfDecPerPerson = CostOfFancyDecPerPerson;
+                    fFlatFeeDecoration = FlatFeeFancyDec;
+                    break;
+                case false:
+                    fCostOfDecPerPerson = CostOfNormalDecPerPerson;
+                    fFlatFeeDecoration = FlatFeeNormalDec;
+                    break;
+            }
+        }
+
+        public decimal TotalCost()
+        {
+            decimal sum = FoodChargePerPerson * NumberOfPeople;
+            sum += fCostOfBevPerPerson * NumberOfPeople;
+            sum += fCostOfDecPerPerson * NumberOfPeople + fFlatFeeDecoration;
+            sum *= (decimal)fDiscount;
+
+            if (NumberOfPeople == 0)
+                sum = 0;
+
+            return sum;
+        }
+}
+}
