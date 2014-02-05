83b51296455c6203d1eb057dc429ff5918739c12
John Walley <john.walley@john.walley>
2014-02-05 11:02:54 +0000

    New unit test project

---
+++ DataFrame/DataFrameTest/DataFrameTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace DataFrameTest
+{
+    [TestClass]
+    public class DataFrameTest
+    {
+        [TestMethod]
+        public void DataFrame_SetUp()
+        {
+            var dataFrame = new DataFrame.DataFrame(
+                new List<string>() { "ford", "vauxhall" },
+                new List<string>() { "35932", "35933" },
+                new dynamic[,] { { 1.1, 1.2 }, { 2.1, 2.2 } });
+
+            Assert.AreEqual(2.1, dataFrame["ford", "35933"], 1e-6);
+        }
+    }
+}
